a7a36d675b2cbb7fa6564965d08d73970fdf169f
Satyam Rathor <104068640+Satyam1923@users.noreply.github.com>
2024-04-03 12:23:19 +0530

    remove geoapi

--- index.js
+++ index.js
@@ -16,13 +16,8 @@ app.get("/", (req, res) => {
 app.post("/search", async (req, res) => {
     try {
         const city = req.body.city;
-        const response1 = await axios.get(
-            `https://api.geoapify.com/v1/geocode/autocomplete?text=${city}&apiKey=c7e86aceeb2d4158a30f0cdac7d7484f`
-        );
-        const lat = response1.data.features[0].properties.lat;
-        const lon = response1.data.features[0].properties.lon;
         const response2 = await axios.get(
-            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a0d3b55486b2f022d3bc97adb89cc47e&units=metric`
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a0d3b55486b2f022d3bc97adb89cc47e&units=metric`
         );
         const weatherData = response2.data;
         console.log(response2.data);
