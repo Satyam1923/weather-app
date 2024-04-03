e03998023f29536d3fb022bc9900926ee7eaa38e
Satyam Rathor <104068640+Satyam1923@users.noreply.github.com>
2024-04-03 12:57:11 +0530

    udpate index.ejs and index.ejs

--- index.js
+++ index.js
@@ -8,26 +8,24 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-let str="";
+
 app.get("/", (req, res) => {
-    res.render("index.ejs",{data:str});
+    res.render("index.ejs", { data: null });
 });
 
 app.post("/search", async (req, res) => {
     try {
         const city = req.body.city;
-        const response2 = await axios.get(
+        const response = await axios.get(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a0d3b55486b2f022d3bc97adb89cc47e&units=metric`
         );
-        const weatherData = response2.data;
-        console.log(response2.data);
-        res.render("index.ejs", {
-            data: weatherData,
-        });
+        const weatherData = response.data;
+        res.render("index.ejs",{data:weatherData});
     } catch (error) {
         console.error("Failed to make request:", error.message);
         res.render("index.ejs", {
-            error: "No weather data available.",
+            error: "Error fetching weather data. Please try again later.",
+            data:'invalid',
         });
     }
 });
